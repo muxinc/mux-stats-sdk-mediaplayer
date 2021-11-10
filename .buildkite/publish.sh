6528bafb35d1105daf43d0f99d633649008fe233
nbirkenshawmux <nbirkenshaw@mux.com>
2021-11-10 15:59:30 -0500

    Removing buildkite gating altogether

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -12,7 +12,7 @@ docker run -it -v --rm  \
     -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
     -w /data \
     muxinc/mux-exoplayer:20201215 \
-    bash -c "./gradlew --info MuxMediaPlayer:artifactoryPublish"
+    bash -c "./gradlew --info artifactoryPublish"
 
 docker run -it -v --rm  \
     -v $(pwd):/data \
