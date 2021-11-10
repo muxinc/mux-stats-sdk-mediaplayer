a5ab78f3271ed1cbc574b4bfc105fe49dfbab02e
nbirkenshawmux <nbirkenshaw@mux.com>
2021-11-10 16:09:20 -0500

    Changes

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -2,17 +2,7 @@
 #
 # Accessing the secret containing env vars in here prevents buildkite from capturing them
 
-docker run -it -v --rm  \
-    -v $(pwd):/data \
-    -e BUILDKITE_BRANCH="$BUILDKITE_BRANCH" \
-    -e ORG_GRADLE_PROJECT_signingKeyId="$ORG_GRADLE_PROJECT_signingKeyId" \
-    -e ORG_GRADLE_PROJECT_signingPassword="$ORG_GRADLE_PROJECT_signingPassword" \
-    -e ORG_GRADLE_PROJECT_signingKey="$ORG_GRADLE_PROJECT_signingKey" \
-    -e ORG_GRADLE_PROJECT_artifactory_user=$ORG_GRADLE_PROJECT_artifactory_user \
-    -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
-    -w /data \
-    muxinc/mux-exoplayer:20201215 \
-    bash -c "./gradlew --info MuxMediaPlayer:artifactoryPublish"
+echo Building library to test
 
 docker run -it -v --rm  \
     -v $(pwd):/data \
@@ -24,4 +14,18 @@ docker run -it -v --rm  \
     -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
     -w /data \
     muxinc/mux-exoplayer:20201215 \
-    bash -c "./gradlew --info automatedtests:assemble automatedtests:assembleAndroidTest"
+    bash -c "./gradlew --info artifactoryPublish"
+
+echo Building and running test suite
+
+docker run -it -v --rm  \
+    -v $(pwd):/data \
+    -e BUILDKITE_BRANCH="$BUILDKITE_BRANCH" \
+    -e ORG_GRADLE_PROJECT_signingKeyId="$ORG_GRADLE_PROJECT_signingKeyId" \
+    -e ORG_GRADLE_PROJECT_signingPassword="$ORG_GRADLE_PROJECT_signingPassword" \
+    -e ORG_GRADLE_PROJECT_signingKey="$ORG_GRADLE_PROJECT_signingKey" \
+    -e ORG_GRADLE_PROJECT_artifactory_user=$ORG_GRADLE_PROJECT_artifactory_user \
+    -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
+    -w /data \
+    muxinc/mux-exoplayer:20201215 \
+    bash -c "./gradlew --info assemble assembleAndroidTest"
