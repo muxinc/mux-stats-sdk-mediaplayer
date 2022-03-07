405dfd79fc0c9e6c78acaf6ac991fce085858d31
Emily Dixon <edixon@mux.com>
2022-03-07 11:05:17 -0800

    Update Docker image IDs

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -13,7 +13,7 @@ docker run -it -v --rm  \
     -e ORG_GRADLE_PROJECT_artifactory_user=$ORG_GRADLE_PROJECT_artifactory_user \
     -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
     -w /data \
-    muxinc/mux-exoplayer:20201215 \
+    muxinc/mux-exoplayer:20220112 \
     bash -c "./gradlew --info artifactoryPublish"
 
 echo Building and running test suite
@@ -27,5 +27,5 @@ docker run -it -v --rm  \
     -e ORG_GRADLE_PROJECT_artifactory_user=$ORG_GRADLE_PROJECT_artifactory_user \
     -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
     -w /data \
-    muxinc/mux-exoplayer:20201215 \
+    muxinc/mux-exoplayer:20220112 \
     bash -c "./gradlew --info assemble assembleAndroidTest"
